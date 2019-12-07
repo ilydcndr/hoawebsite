e262aabd5b963255df9d64815630c119523be003
ilydcndr <ilaydacundar@hotmail.com>
2019-12-07 15:05:19 +0200

    Updates

---
+++ houseofapps.js
@@ -0,0 +1,56 @@
+ AOS.init({
+     duration:1000,
+     once:true,
+ })
+
+ $('.carousel').carousel({
+   interval: 1
+ })
+ 
+ /*  CANCELED!
+ var kaynaklar =[
+    "vakiffoto.html",
+    "dominosfoto.html",
+    "reckitt.html",   
+ ]
+
+  function yoket(){
+     var wearehere=document.getElementById("wearehere");
+     wearehere.style.display="none";
+  }
+
+ i=0;
+   var tdler=document.getElementsByTagName("td");
+   var w3=document.getElementsByClassName("w3");
+   tdler[0].style.fontWeight="900";
+   w3[0].style.opacity="0";
+   
+ function ileri(){
+   var iframe=document.getElementsByTagName("iframe");
+   i++;
+   if(i<3 && i>0){
+      iframe[0].src=kaynaklar[i];
+      tdler[i].style.fontWeight="900";
+      tdler[i-1].style.fontWeight="lighter";
+   }
+   if(i==3){
+      tdler[i-1].style.fontWeight="lighter";
+      i=0;
+   } 
+   tdler[i].style.fontWeight="900";
+      iframe[0].src=kaynaklar[i];
+} 
+ function geri(){
+   var iframe=document.getElementsByTagName("iframe");
+    i--;
+    if(i<0){
+       i=kaynaklar.length-1;
+    }
+       iframe[0].src=kaynaklar[i];   
+ }*/
+ 
+
+    
+
+
+
